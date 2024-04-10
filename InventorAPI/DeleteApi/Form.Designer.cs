1084ecb796217b0b55c8e5b02d293495a56a453b
SonaliPatankar <158050645+SonaliPatankar@users.noreply.github.com>
2024-04-10 17:01:34 +0530

    Update Form.Designer.cs

--- InventorAPI/DeleteApi/Form.Designer.cs
+++ InventorAPI/DeleteApi/Form.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+        
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,7 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+        
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
@@ -39,9 +29,9 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Delete";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button_Click);
             // 
-            // Form1
+            // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
